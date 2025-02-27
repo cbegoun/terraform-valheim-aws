dc8cebb5a398e2a0ab772659ba832f3019549313
cbegoun <begouncody@gmail.com>
2025-02-27 12:40:43 -0700

    idk anymore

--- outputs.tf
+++ outputs.tf
@@ -11,12 +11,12 @@ resource "local_file" "private_key" {
   filename          = "${path.module}/private_key.pem"
 }
 
+output "private_key_pem" {
+  value     = tls_private_key.valheim.private_key_pem
+  sensitive = true
+}
+
 output "private_key_file" {
   value     = local_file.private_key.filename
   sensitive = true
 }
-
-output "public_key_openssh" {
-  value = tls_private_key.valheim.public_key_openssh
-}
-
