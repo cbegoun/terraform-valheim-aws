bcefd8e31b748b7c1bbf1bd11be8c5356a117c07
cbegoun <begouncody@gmail.com>
2025-02-27 12:06:34 -0700

    stop being stupid

--- outputs.tf
+++ outputs.tf
@@ -11,11 +11,12 @@ resource "local_file" "private_key" {
   filename          = "${path.module}/private_key.pem"
 }
 
+output "private_key_file" {
+  value     = local_file.private_key.filename
+  sensitive = true
+}
+
 output "public_key_openssh" {
   value = tls_private_key.valheim.public_key_openssh
 }
 
-resource "local_file" "private_key" {
-  sensitive_content = tls_private_key.example.private_key_pem
-  filename          = "${path.module}/private_key.pem"
-}
