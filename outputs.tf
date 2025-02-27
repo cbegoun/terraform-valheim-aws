3786254327ebd3cf62172d348f687e1668db46fe
cbegoun <begouncody@gmail.com>
2025-02-27 09:48:53 -0700

    fix output

--- outputs.tf
+++ outputs.tf
@@ -13,7 +13,7 @@ output "instance_id" {
   description = "The EC2 instance ID"
 }
 output "hosted_zone_nameservers" {
-  value       = aws_route53_zone.minecraft_ondemand_route53_zone.name_servers
+  value       = aws_route53_zone.selected.name_servers
   description = "The Hosted Zone's NS records. Use this to delegate the zone from your parent zone."
 }
 
