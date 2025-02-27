1622bb352270d134300bb86a90e7692a642e03c5
cbegoun <begouncody@gmail.com>
2025-02-26 19:40:34 -0700

    switched to ec2 module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,24 @@
-output "hosted_zone_nameservers" {
-  value       = aws_route53_zone.valheim_ondemand_route53_zone.name_servers
-  description = "The Hosted Zone's NS records. Use this to delegate the zone from your parent zone."
+output "monitoring_url" {
+  value       = module.main.monitoring_url
+  description = "URL to monitor the Valheim Server"
+}
+
+output "bucket_id" {
+  value       = module.main.bucket_id
+  description = "The S3 bucket name"
+}
+
+output "instance_id" {
+  value       = module.main.instance_id
+  description = "The EC2 instance ID"
+}
+
+output "valheim_user_passwords" {
+  value       = module.main.valheim_user_passwords
+  description = "List of AWS users and their encrypted passwords"
+}
+
+output "valheim_server_name" {
+  value       = var.server_name
+  description = "Name of the Valheim server"
 }
