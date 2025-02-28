eb21f077099f0da9b7c573fe8f3c485a58e83a16
Cody Begoun <begouncody@gmail.com>
2025-02-27 17:29:21 -0700

    asdf

--- iam.tf
+++ iam.tf
@@ -1,24 +1,23 @@
-resource "aws_iam_role" "lambda_role" {
-  name               = "lambda_basic_execution"
-  assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
       {
-        "Action": "sts:AssumeRole",
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "lambda.amazonaws.com"
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
         }
       }
     ]
-  }
-  EOF
+  })
 }
-
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  role       = aws_iam_role.lambda_role.name
+  role       = aws_iam_role.lambda_exec_role.name
 }
 
 resource "aws_iam_role" "backup_lambda_role" {
