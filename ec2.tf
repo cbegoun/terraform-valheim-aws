fd36b891f0f2a9019f66c074e2e42146150a9df8
Cody Begoun <begouncody@gmail.com>
2025-04-08 11:38:19 -0600

    preloading script contents

--- ec2.tf
+++ ec2.tf
@@ -28,6 +28,12 @@ resource "aws_key_pair" "valheim_keypair" {
   }
 }
 
+# Preload script contents into local variables
+locals {
+  install_valheim_script = file("${path.module}/scripts/install_valheim.ps1.tftpl")
+  watchdog_script        = file("${path.module}/scripts/watchdog.ps1")
+}
+
 resource "aws_instance" "valheim" {
   ami               = data.aws_ami.windows_2022.id
   instance_type     = "t3.medium"
@@ -37,12 +43,13 @@ resource "aws_instance" "valheim" {
     device_index         = 0
   }
 
-    user_data = base64encode(templatefile("${path.module}/scripts/userdata.ps1.tftpl", {
-        server_name = var.valheim_server_name,
-        world_name  = var.valheim_world_name,
-        server_pass = var.valheim_password
-        path        = path.module
-        }))
+  user_data = base64encode(templatefile("${path.module}/scripts/userdata.ps1.tftpl", {
+    server_name         = var.valheim_server_name,
+    world_name          = var.valheim_world_name,
+    server_pass         = var.valheim_password,
+    install_valheim_ps1 = local.install_valheim_script,  # Pass the content of install_valheim.ps1
+    watchdog_ps1        = local.watchdog_script         # Pass the content of watchdog.ps1
+  }))
 
   tags = {
     Name = "valheim-server"
