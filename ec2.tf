1d778a85695002d878bc0e7801eceaccaaf3a6d4
Cody Begoun <begouncody@gmail.com>
2025-04-08 11:32:24 -0600

    update path

--- ec2.tf
+++ ec2.tf
@@ -41,6 +41,7 @@ resource "aws_instance" "valheim" {
         server_name = var.valheim_server_name,
         world_name  = var.valheim_world_name,
         server_pass = var.valheim_password
+        path        = path.module
         }))
 
   tags = {
